0b7a8464f87a07840ff9db236933f5fe56cb2c1d
AlanYang99 <33969636+AlanYang99@users.noreply.github.com>
2018-09-22 12:44:20 +1000

    Update chatup.py

--- chatup.py
+++ chatup.py
@@ -23,6 +23,5 @@ def handle_my_custom_event( json ):
   socketio.emit( 'my response', json, callback=messageRecived )
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  #socketio.run( app, debug = True )
-  app.run(host='0.0.0.0', port=port, debug=True)
+    socketio.run(app)
+
